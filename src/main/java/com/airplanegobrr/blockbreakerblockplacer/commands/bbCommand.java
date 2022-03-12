54c28d13d88923acf5f48dd310a64a7f4453ef5c
AirplaneGoBrr <71111395+AirplanegoBrr@users.noreply.github.com>
2022-03-11 17:52:54 -0800

    first commit

---
+++ src/main/java/com/airplanegobrr/blockbreakerblockplacer/commands/bbCommand.java
@@ -0,0 +1,79 @@
+package com.airplanegobrr.blockbreakerblockplacer.commands;
+
+import com.airplanegobrr.blockbreakerblockplacer.Blockbreakerblockplacer;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class bbCommand implements CommandExecutor {
+    private final Blockbreakerblockplacer main;
+
+    public bbCommand(Blockbreakerblockplacer main) {
+        this.main = main;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            FileConfiguration config = main.getConfig();
+            Player player = (Player) sender;
+
+            //Check perm of the player
+            if (!player.isOp() && !player.hasPermission("use")) {
+                player.sendMessage(ChatColor.RED+"No perm!");
+                return true;
+            }
+
+            //Get the block that is 5 blocks in front of the player
+            Block b = player.getTargetBlock(null, 5);
+
+            //Add check here to make sure block is dispenser or dropper
+            if (b.getType() != Material.DISPENSER) {
+                player.sendMessage(ChatColor.RED+"You must be looking at a dispenser!");
+                return true;
+            }
+
+            //Get the loc of the block we are looking at
+            Location loc = b.getLocation();
+
+            //Compile that loc to a string (will add world later!)
+            String xyzName = (loc.getBlockX() + "," + loc.getBlockY() + "," + loc.getBlockZ());
+
+            //Set that dealt to be false
+            config.addDefault("blockBreakers." + xyzName, false);
+            //Get the block info
+            boolean con = config.getBoolean("blockBreakers." + xyzName);
+
+            //if the block is false the set it true + the other way around
+
+            //false -> true
+            if (con == false) {
+                // add the block to data.blockBreakers
+                config.set("blockBreakers." + xyzName, true);
+                player.sendMessage("Block added to blockBreakers!");
+                main.getLogger().info("Block added to blockBreakers!");
+
+                //true -> false
+            } else {
+                // remove the block from data.blockBreakers
+                config.set("blockBreakers." + xyzName, false);
+                player.sendMessage("Block removed from blockBreakers!");
+                main.getLogger().info("Block removed from blockBreakers!");
+            }
+            //save data to config??
+            main.saveConfig();
+            return true;
+
+        } else {
+            main.getLogger().info("Player only command!");
+            return true;
+        }
+    }
+}
---
+++ src/main/java/com/airplanegobrr/blockbreakerblockplacer/events/onBlockDispense.java
@@ -0,0 +1,90 @@
+package com.airplanegobrr.blockbreakerblockplacer.events;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.Dispenser;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDispenseEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.block.data.Directional;
+
+import com.airplanegobrr.blockbreakerblockplacer.Blockbreakerblockplacer;
+
+public class onBlockDispense implements Listener {
+    private final Blockbreakerblockplacer main;
+
+    public onBlockDispense(Blockbreakerblockplacer main) {
+        this.main = main;
+    }
+
+    @EventHandler
+    public void BlockDispenseEvent(BlockDispenseEvent event) {
+        main.getLogger().info("Block dispensed!");
+        Block block = event.getBlock();
+        Location loc = block.getLocation();
+        String xyzName = (loc.getBlockX() + "," + loc.getBlockY() + "," + loc.getBlockZ());
+        ItemStack item = event.getItem();
+        Material itemType = item.getType();
+        Dispenser dispenser = (Dispenser) block.getState();
+
+        // Config stuff
+        main.getConfig().addDefault("debug", false);
+        boolean debug = main.getConfig().getBoolean("debug");
+
+        main.getConfig().addDefault("blockBreakers." + xyzName, false);
+        boolean bbEnabled = main.getConfig().getBoolean("blockBreakers." + xyzName);
+
+        main.getConfig().addDefault("blockPlacers." + xyzName, false);
+        boolean bpEnabled = main.getConfig().getBoolean("blockPlacers." + xyzName);
+
+        // Real stuff
+
+        // blockbreaker
+        if (bbEnabled) {
+            if (debug) {
+                main.getLogger().info(item.toString());
+                main.getLogger().info(item.getType().toString());
+                main.getLogger().info(item.getType().name());
+                main.getLogger().info(xyzName);
+            }
+
+            if (item.getType().toString().contains("PICKAXE")) {
+                main.getLogger().info("Pickaxe detected!");
+                BlockFace bsFace = ((Directional) block.getBlockData()).getFacing();
+                Block bsBlock = block.getRelative(bsFace);
+                Material bsMaterial = bsBlock.getType();
+                // if the material is not bedrock then break it
+                if (bsMaterial != Material.BEDROCK) {
+                    bsBlock.breakNaturally();
+                    event.setCancelled(true);
+                }
+            }
+        }
+
+        // blockplacer
+        if (bpEnabled) {
+            if (debug) {
+                main.getLogger().info(item.toString());
+                main.getLogger().info(item.getType().toString());
+                main.getLogger().info(item.getType().name());
+                main.getLogger().info(xyzName);
+            }
+
+            if (itemType.isBlock()){
+                BlockFace bsFace = ((Directional) block.getBlockData()).getFacing();
+                Block bsBlock = block.getRelative(bsFace);
+                Material bsMaterial = bsBlock.getType();
+                
+                if (bsMaterial == Material.AIR || bsMaterial == Material.WATER || bsMaterial == Material.LAVA) {
+                    bsBlock.setType(itemType);
+                    event.setCancelled(true);
+                    dispenser.getInventory().removeItem(item);
+                }
+            }
+        }
+
+    }
+}
